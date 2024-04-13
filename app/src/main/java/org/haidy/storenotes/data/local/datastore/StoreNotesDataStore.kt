b9080fd53121080b64b143afc6b3bb2314df737d
haidy <haidyabugom3a@gmail.com>
2024-04-13 04:38:35 +0200

    implement data store

---
+++ app/src/main/java/org/haidy/storenotes/data/local/datastore/StoreNotesDataStore.kt
@@ -0,0 +1,46 @@
+package org.haidy.storenotes.data.local.datastore
+
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.booleanPreferencesKey
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.stringPreferencesKey
+import androidx.datastore.preferences.preferencesDataStore
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.map
+import javax.inject.Inject
+
+class StoreNotesDataStore @Inject constructor(context: Context) {
+    companion object {
+        private const val PREFERENCES_FILE_NAME = "store_notes"
+        private val USER_ID = stringPreferencesKey("user_id")
+        private val IS_LOGGED = booleanPreferencesKey("is_logged")
+    }
+
+    private val Context.preferencesDataStore: DataStore<Preferences> by preferencesDataStore(
+        PREFERENCES_FILE_NAME
+    )
+    private val prefDataStore = context.preferencesDataStore
+    suspend fun saveUserId(userId: String) {
+        prefDataStore.edit { preferences ->
+            preferences[USER_ID] = userId
+        }
+    }
+
+    suspend fun getUserId(): String {
+        return prefDataStore.data.map { preferences -> preferences[USER_ID] }.first() ?: ""
+    }
+
+    suspend fun saveIsLoggedIn(isLogged: Boolean) {
+        prefDataStore.edit { preferences ->
+            preferences[IS_LOGGED] = isLogged
+        }
+    }
+
+    suspend fun getIsLoggedIn(): Boolean {
+        return prefDataStore.data.map { preferences -> preferences[IS_LOGGED] }.first() ?: false
+    }
+
+
+}
