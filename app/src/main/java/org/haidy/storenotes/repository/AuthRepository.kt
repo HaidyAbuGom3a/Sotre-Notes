74ad8512f4974daea547ade7c23275aa0dcde3ec
haidy <haidyabugom3a@gmail.com>
2024-04-13 14:12:22 +0200

    implement auth repository

--- app/src/main/java/org/haidy/storenotes/repository/AuthRepository.kt
+++ app/src/main/java/org/haidy/storenotes/repository/AuthRepository.kt
@@ -1,6 +1,31 @@
 package org.haidy.storenotes.repository
 
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.coroutines.tasks.await
 import javax.inject.Inject
+import kotlin.coroutines.cancellation.CancellationException
 
-class AuthRepository @Inject constructor() {
+class AuthRepository @Inject constructor(private val auth: FirebaseAuth) {
+    suspend fun signUpUser(
+        email: String,
+        password: String
+    ): Boolean {
+        return try {
+            val request = auth.createUserWithEmailAndPassword(email, password).await()
+            request.user?.uid != null
+        } catch (e: Exception) {
+            if (e is CancellationException) throw e
+            throw e
+        }
+    }
+
+    suspend fun loginUser(email: String, password: String): String {
+        return try {
+            val request = auth.signInWithEmailAndPassword(email, password).await()
+            request?.user?.uid ?: ""
+        } catch (e: Exception) {
+            if (e is CancellationException) throw e
+            throw e
+        }
+    }
 }
