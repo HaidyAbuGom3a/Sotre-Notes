7bca36df4b1e4b7aee73629c2f4c8ea0b0dfbe17
haidy <haidyabugom3a@gmail.com>
2024-04-13 14:15:35 +0200

    implement user repository

---
+++ app/src/main/java/org/haidy/storenotes/repository/UserRepository.kt
@@ -0,0 +1,25 @@
+package org.haidy.storenotes.repository
+
+import org.haidy.storenotes.data.local.datastore.StoreNotesDataStore
+import javax.inject.Inject
+
+class UserRepository @Inject constructor(private val dataStore: StoreNotesDataStore) {
+
+    suspend fun saveUserId(userId: String) {
+        dataStore.saveUserId(userId)
+    }
+
+    suspend fun getUserId(): String {
+        return dataStore.getUserId()
+    }
+
+    suspend fun saveIsLoggedIn(isLogged: Boolean) {
+        dataStore.saveIsLoggedIn(isLogged)
+    }
+
+    suspend fun getIsLoggedIn(): Boolean {
+        return dataStore.getIsLoggedIn()
+    }
+
+
+}
